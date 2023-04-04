3a262c25d8397cae22f1607a6a88251158f9fcde
Kamisi Pamilerin <kamisipamilerin2004@gmail.com>
2023-04-04 11:31:23 +0100

    Made some changes

--- index.js
+++ index.js
@@ -1,90 +1,26 @@
-// var first = document.getElementById("first");
-// var second = document.getElementById("second");
-// var third = document.getElementById("third");
-// var fourth = document.getElementById("fourth");
-// var fifth = document.getElementById("fifth");
+window.onscroll = function () {
+  scrollFunction();
+};
 
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-// window.onscroll = function () {
-//   scrollFunction("two");
-// };
-// window.onscroll = function () {
-//   scrollFunction("three");
-// };
-// window.onscroll = function () {
-//   scrollFunction("four");
-// };
-// window.onscroll = function () {
-//   scrollFunction("five");
-// };
+var topnav = document.getElementById("topnav");
 
-// function scrollFunction() {
-// if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     first.style.width = "10px";
-//     first.style.height = "10px";
-//     first.style.background = "rgb(190, 34, 34)";
-//   } else {
-//     first.style.width = "7px";
-//     first.style.height = "7px";
-//     first.style.background = "#fff";
-//   }
-// }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topnav.style.background = "rgb(0,0,0,0.9)";
+    topnav.style.transition = ".4s";
+  } else {
+    topnav.style.background = "none";
+    topnav.style.transition = ".4s";
+  }
+}
 
-// function scrollFunction() {
-//   if (
-//     document.body.scrollTop > 420 ||
-//     document.documentElement.scrollTop > 420
-//   ) {
-//     second.style.width = "10px";
-//     second.style.height = "10px";
-//     second.style.background = "rgb(190, 34, 34)";
-//   } else {
-//     second.style.width = "7px";
-//     second.style.height = "7px";
-//     second.style.background = "#fff";
-//   }
-// }
-// // function scrollFunction() {
-// //   if (
-// //     document.body.scrollTop > 820 ||
-// //     document.documentElement.scrollTop > 820
-// //   ) {
-// //     third.style.width = "10px";
-// //     third.style.height = "10px";
-// //     third.style.background = "rgb(190, 34, 34)";
-// //   } else {
-// //     third.style.width = "7px";
-// //     third.style.height = "7px";
-// //     third.style.background = "#fff";
-// //   }
-// // }
-// // function scrollFunction() {
-// //   if (
-// //     document.body.scrollTop > 1220 ||
-// //     document.documentElement.scrollTop > 1220
-// //   ) {
-// //     fourth.style.width = "10px";
-// //     fourth.style.height = "10px";
-// //     fourth.style.background = "rgb(190, 34, 34)";
-// //   } else {
-// //     fourth.style.width = "7px";
-// //     fourth.style.height = "7px";
-// //     fourth.style.background = "#fff";
-// //   }
-// // }
-// // function scrollFunction() {
-// //   if (
-// //     document.body.scrollTop > 1620 ||
-// //     document.documentElement.scrollTop > 1620
-// //   ) {
-// //     fifth.style.width = "10px";
-// //     fifth.style.height = "10px";
-// //     fifth.style.background = "rgb(190, 34, 34)";
-// //   } else {
-// //     fifth.style.width = "7px";
-// //     fifth.style.height = "7px";
-// //     fifth.style.background = "#fff";
-// //   }
-// // }
+var sidenav = document.getElementById("sidenav");
+
+function opennav() {
+  sidenav.style.position = "fixed";
+  sidenav.style.right = "0px";
+}
+function closenav() {
+  sidenav.style.position = "fixed";
+  sidenav.style.right = "-250px";
+}
